3af0ed8f901b3747346f1a59ba9617383b207a8b
davidechon <davidechon@gmail.com>
2023-10-26 18:59:26 +0200

    wip filter

--- js/script.js
+++ js/script.js
@@ -132,24 +132,38 @@ const icons = [
 	}
 ];
 
+// let litTemplate = "";
+// icons.forEach((element) =>{
+//   litTemplate =`
+//         <div class="icons">
+//           <div class="filterDiv ${element.type}"><i class="${element.family} ${element.prefix + element.name} ${element.color}"></i></div>
+//           <p class="etichetta">${element.name.toUpperCase()}</p>
+//         </div>
+//       `;
+//       app.innerHTML += litTemplate;
+//       // console.log(litTemplate);
+// });
+
 let litTemplate = "";
 icons.forEach((element) =>{
   litTemplate =`
-        <div class="icons">
-          <span><i class="${element.family} ${element.prefix + element.name} ${element.color}"></i></span>
+        <div class="filterDiv ${element.type}">
+          <i class="${element.family} ${element.prefix + element.name} ${element.color}"></i>
           <p class="etichetta">${element.name.toUpperCase()}</p>
         </div>
-      `;
-      app.innerHTML += litTemplate;
+				`;
+      // app.innerHTML += litTemplate;
+      myBtnContainer.innerHTML += litTemplate;
       // console.log(litTemplate);
 });
-
-
 //////////////////////////////////////////////////
 // Milestone 3
 // Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
-// function printAllIcons(filteredIcon){
-// 	filteredIcon.forEach((icona)=>{
+// let filteredIcon;
+// let icona;
+
+// function printAllIcons(filteredIcon) {
+// 	filteredIcon.forEach((icona) => {
 // 		const box = document.createElement('div');
 // 		box.setAttribute('class', 'box');
 // 		box.innerHTML = `
@@ -157,7 +171,7 @@ icons.forEach((element) =>{
 // 		<div class="etichetta">${element.name.toUpperCase()}</div>
 // 		`
 // 		// console.log(box);
-// 	})
+// 	});
 // 	container.append(box);
 // }
 // ----------------------------------------------------------
@@ -174,25 +188,81 @@ icons.forEach((element) =>{
 // printAllIcons(filteredIcons);
 // ----------------------------------------------------------
 
-function filter() {
-	let elementoFiltrato = document.getElementById("options").value;
-	document.getElementById("demo").innerHTML = "You selected: " + elementoFiltrato;
+//  <select id="options" onchange="filterSelection()"></select>
 
-	if (elementoFiltrato === icons.type) {
-		
-		icons.forEach((elementoFiltrato) =>{
-			litTemplate =`
-						<div class="icons">
-							<span><i class="${element.family} ${element.prefix + element.name} ${element.color}"></i></span>
-							<p class="etichetta">${element.name.toUpperCase()}</p>
-						</div>
-					`;
-					app.innerHTML += litTemplate;
-					console.log(elementoFiltrato);
-	});
-
-
-
+filterSelection("all")
+function filterSelection(c) {
+	let x, i;
+	x = document.getElementsByClassName("filterDiv");
+	if (c === "all") c = " ";
+	// Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+	for (i = 0; i < x.length; i++) {
+		myRemoveClass(x[i], "show");
+		if (x[i].className.indexOf(c) > -1) myAddClass(x[i], "show");
 	}
 }
 
+// Show filtered elements
+function myAddClass(element, name) {
+	let i, arr1, arr2;
+	arr1 = element.className.split(" ");
+	arr2 = name.split(" ");
+	for (i = 0; i < arr2.length; i++) {
+		if (arr1.indexOf(arr2[i]) == -1) {
+			element.className += " " + arr2[i];
+		}
+	}
+}
+
+// Hide elements that are not selected
+function myRemoveClass(element, name) {
+	let i, arr1, arr2;
+	arr1 = element.className.split(" ");
+	arr2 = name.split(" ");
+	for (i = 0; i < arr2.length; i++) {
+		while (arr1.indexOf(arr2[i]) > -1) {
+			arr1.splice(arr1.indexOf(arr2[i]), 1);
+		}
+	}
+	element.className = arr1.join(" ");
+}
+
+
+// Add active class to the current control button (highlight it)
+var optionList = document.getElementById("myBtnContainer");
+var btns = optionList.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+// ----------------------------------------------------------
+// Stampo l'html corrispondente nel contenitore relativo
+// const eventiContainer = document.querySelector(".eventi-container");
+// eventiContainer.innerHTML = eventoTemplate;
+
+// // Funzione searchTitle
+// function searchTitle() {
+// let inputText; 
+//   let filter;
+//   let eventList;
+//   let eventItem;
+//   let x;
+//   let textSearch;
+//    inputText = document.getElementById("searchBar");
+//    filter = inputText.value.toUpperCase();
+//    eventList = document.getElementById("elenco");
+//    eventItem = eventList.getElementsByTagName("li");
+//   for (let i = 0; i < eventItem.length; i++) {
+//      x = eventList.getElementsByTagName("li")[i];
+//      textSearch = x.textContent || x.innerText;
+//     if (textSearch.toUpperCase().indexOf(filter) > -1) {
+//       eventItem[i].style.display = "";
+//     } else {
+//       eventItem[i].style.display = "none";
+//     }
+//   }
+// }
